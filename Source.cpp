bffb7dcbb23379b74d34aa47ad0c7fd4cc3df143
timmmmut <timurberez@gmail.com>
2020-11-24 17:16:34 +0300

    add file

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,13 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
+	ofstream outFile;
+	outFile.open("my_file.txt");
+	string str;
+	getline(cin, str);
+	outFile << str;
+	outFile.close();
 	return 0;
 }
